ec789752d4c90799760e15114385757c9524a510
alireza-khorami-mint <alirezakhoramimn@gmail.com>
2020-11-17 14:15:32 +0330

    fxing

--- elmamot/settings.py
+++ elmamot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_DJANGO_KEY'
+SECRET_KEY = os.environ.get('SECRET_DJANGO_KEY'
  # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
