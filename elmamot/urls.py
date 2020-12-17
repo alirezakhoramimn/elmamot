0c6e24a90e27f930db144df3bfcc7540c8674e74
alirezakhoramimn <alirezakhoramimn@gmail.com>
2020-12-17 13:34:18 +0330

    fixed the urls

--- elmamot/urls.py
+++ elmamot/urls.py
@@ -1,18 +1,3 @@
-"""elmamot URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -20,12 +5,12 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from language.views import home as home_lang
 from language.sitemaps import LanguageSitemap,FrameWorkSitemap
-<<<<<<< HEAD
+
 from rest_framework import routers
-=======
+
 
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
->>>>>>> 677b20f59146d8d0ae020ad6e985d1bc0cad7ef2
+
 from graphene_django.views import GraphQLView
 import language.views as lang_views 
 router = routers.DefaultRouter()
@@ -53,16 +38,12 @@ urlpatterns = [
 
     path('question/', include('quiz.urls')),
 
-<<<<<<< HEAD
     path('graphql/', GraphQLView.as_view(graphiql=True)),
     # Rest 
     path('rest/', include('rest_framework.urls')),
-=======
+
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 
-    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
->>>>>>> 677b20f59146d8d0ae020ad6e985d1bc0cad7ef2
-]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
